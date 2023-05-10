a07949af79bb403899a9fa49906ad276b5ea9d30
Artur Yakupov <art.kazan@mail.ru>
2023-05-10 18:38:22 +0300

    upgraded EFCore, changed webhosting configuration

--- Tests/WebAPI.Tests/TestApplicationFactory.cs
+++ Tests/WebAPI.Tests/TestApplicationFactory.cs
@@ -11,33 +11,27 @@ public class TestApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        base.ConfigureWebHost(builder);
         builder.ConfigureServices(services =>
         {
             var dbDescriptor = services.SingleOrDefault(
                 descriptor => descriptor.ServiceType == typeof(DbContextOptions<AppDbContext>));
             services.Remove(dbDescriptor!);
             
-            var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkInMemoryDatabase()
-                .BuildServiceProvider();
             
             var nameTestDb = Guid.NewGuid().ToString();
             services.AddDbContext<AppDbContext>(
                 optionsBuilder => optionsBuilder.UseInMemoryDatabase(nameTestDb));
             
             var sp = services.BuildServiceProvider();
-
             using var scope = sp.CreateScope();
             var scopedServices = scope.ServiceProvider;
             var db = scopedServices.GetRequiredService<AppDbContext>();
-
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-
+            
             InitDataBase(db);
-        }); 
+        });
     }
-
+    
     private void InitDataBase(AppDbContext context)
     {
         context.UserGroups.AddRange(new UserGroup { Role = Role.Admin, Description = "Admin" },
